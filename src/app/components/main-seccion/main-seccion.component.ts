b6caa1ebeb0da93463bc1184e7d0c78ef33c56b0
Leonel Dimasi <leoneldimasi@gmail.com>
2020-11-21 19:10:13 -0300

    swiper

--- src/app/components/main-seccion/main-seccion.component.ts
+++ src/app/components/main-seccion/main-seccion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import Swiper from 'swiper';
+// import Swiper styles
+//import 'swiper/swiper-bundle.css';
 @Component({
   selector: 'app-main-seccion',
   templateUrl: './main-seccion.component.html',
@@ -10,6 +12,40 @@ export class MainSeccionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    /*let swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });*/
+
+    let swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      loop: true,
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
   }
 
 }
