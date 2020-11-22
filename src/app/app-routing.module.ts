34a13d9d223373dc46474379bd4845dddb2bca5a
FerBarragan10 <fer7_10@hotmail.com>
2020-11-22 00:52:59 -0300

    cambios en el login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from '../app/components/home/home.component';
 import { LoginComponent } from '../app/components/login/login.component';
 
 
 const routes: Routes = [
-  {path:'home',component:HomeComponent},
+  //{path:'home',component:HomeComponent},
   {path:'login',component:LoginComponent},
   {path:'**',pathMatch:'full',redirectTo:'login'},
 ];
