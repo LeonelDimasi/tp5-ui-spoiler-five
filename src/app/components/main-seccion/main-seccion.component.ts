0dde4bb9ec34289303fc6f3aa9061ad460c24758
Leonel Dimasi <leoneldimasi@gmail.com>
2020-11-30 19:00:20 -0300

    ddsfsdf

--- src/app/components/main-seccion/main-seccion.component.ts
+++ src/app/components/main-seccion/main-seccion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import Swiper from 'swiper';
 // import Swiper styles
 //import 'swiper/swiper-bundle.css';
@@ -9,26 +10,52 @@ import Swiper from 'swiper';
 })
 export class MainSeccionComponent implements OnInit {
 
-  constructor() { }
+  playlists= [
+    {
+      id:1,
+      nombre:"musica para codear",
+      urlImg:"https://i1.wp.com/con2bemolesradio.com/wp-content/uploads/2017/11/ROCK.jpg?fit=350%2C401&ssl=1",
+      creador:"juan perez",
+
+    },
+    {
+      id:1,
+      nombre:"musica para codear",
+      urlImg:"https://i1.wp.com/con2bemolesradio.com/wp-content/uploads/2017/11/ROCK.jpg?fit=350%2C401&ssl=1",
+      creador:"juan perez",
+
+    },
+    {
+      id:1,
+      nombre:"musica para codear",
+      urlImg:"https://i1.wp.com/con2bemolesradio.com/wp-content/uploads/2017/11/ROCK.jpg?fit=350%2C401&ssl=1",
+      creador:"juan perez",
+
+    },
+    {
+      id:1,
+      nombre:"musica para codear",
+      urlImg:"https://i1.wp.com/con2bemolesradio.com/wp-content/uploads/2017/11/ROCK.jpg?fit=350%2C401&ssl=1",
+      creador:"juan perez",
+
+    },
+    {
+      id:1,
+      nombre:"musica para codear",
+      urlImg:"https://i1.wp.com/con2bemolesradio.com/wp-content/uploads/2017/11/ROCK.jpg?fit=350%2C401&ssl=1",
+      creador:"juan perez",
+
+    }
+  ];
+
+  constructor(private router:Router) { 
+   
+    
+  }
 
   ngOnInit(): void {
-    /*let swiper = new Swiper('.swiper-container', {
-      effect: 'coverflow',
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: 'auto',
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows : true,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-      },
-    });*/
-
+   
+   setTimeout(() => {
     let swiper = new Swiper('.swiper-container', {
       effect: 'coverflow',
       loop: true,
@@ -46,6 +73,14 @@ export class MainSeccionComponent implements OnInit {
         el: '.swiper-pagination',
       },
     });
+   }, 200);
+
+    
   }
 
+
+  irA(id:number){
+    this.router.navigateByUrl('[playlist/'+id+']')
+    //[routerLink]="['/playlist/'{{playlist.id}}]" 
+  }
 }
